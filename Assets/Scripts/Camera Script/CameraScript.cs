a1e3eba31865a1d1e5a8890dc98642970e07c8d7
Viditv68 <38073713+Viditv68@users.noreply.github.com>
2020-02-13 06:44:10 +0530

    Initial commit

---
+++ Assets/Scripts/Camera Script/CameraScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public static float offsetX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BirdScript.instance != null)
+        {
+            if (BirdScript.instance.isAlive)
+                MoveTheCamera();
+                
+        }
+        
+    }
+
+    void MoveTheCamera()
+    {
+        Vector3 tmp = transform.position;
+        tmp.x = BirdScript.instance.GetPositionX() + offsetX;
+        transform.position = tmp;
+    }
+}
